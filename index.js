48d29e12c1391a9cc6f8ae553c5b6502334a7418
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-07-11 13:38:18 +0400

    add dotenv package and configured

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const server = require('./api/server.js');
+require("dotenv").config();
 
-const port = process.env.PORT || 4000;
+const server = require("./api/server.js");
+
+const port = process.env.PORT;
 
 server.listen(port, () => {
   console.log(`*** Server Running on http://localhost:${port} ***`);
